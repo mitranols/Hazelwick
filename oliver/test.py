628c21e93a4e200a181bc260e48e7c8b1dc604b5
mitranols <21MITRANOl@hazelwick.org>
2026-09-17 11:18:44 +0000

    17/09/26

--- oliver/test.py
+++ oliver/test.py
@@ -1,4 +1,8 @@
-name = input("What is your name")
+name = input("What is your name? ")
 height = float(input("Enter your height in cm: "))
 metre = height / 100
-inch
+INCH = 2.54
+print(f"Hi {name.title()}!")
+print(f"Your height is {height} m.")
+print(f"That is {round((height / 2.54), 2)} inches.")
+print(f"Taller than 180 cm: {height>180}")
