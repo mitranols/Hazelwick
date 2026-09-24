dec9ecb545f92c998bf05bb88fde007a430ee63e
mitranols <21MITRANOl@hazelwick.org>
2026-09-24 11:43:02 +0000

    24/09/26

--- oliver/section 4/lyear.py
+++ oliver/section 4/lyear.py
@@ -1,26 +1,19 @@
-year = int(input("Enter the year"))
+year = int(input("Enter the year: "))
 isleap = 0
 # leap year solve
-if year % 4 == 0 :
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("Leap year")
-            isleap = 1
-        else:
-            print("Not leap year")
-    else:
-        print("Leap year")
-        isleap = 1
+if year % 4 == 0 or (year % 4 == 0 and year % 100 == 0 and year % 400):
+    print(f"Year {year} is a leap year.")
+    isleap = 1
 else:
-    print("Not Leap year")
+    print(f"Year {year} is not a leap year.")
 # month days
 month = int(input("Enter a month number (1-12): "))
 days = 0
 if month == 2 and isleap == 1:
     days = 29
-elif month == 2 and isleap != 0:
+elif month == 2 and isleap != 1:
     days = 28
-elif month == 4 or 6 or 9 or 11:
+elif month == (4 or 6 or 9 or 11):
     days = 30
 else:
     days = 31
